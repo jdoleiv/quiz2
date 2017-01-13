0f298f4b5995959f5915d27b009e2af4798075b1
jdoleiv <jdoleiv@gmail.com>
2017-01-13 15:10:38 +0800

    Movie controller create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,14 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    Movie.create(movie_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :director)
+  end
 end
